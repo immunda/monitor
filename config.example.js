7bf451de8710a9ccf83db08c972171a411fe559a
Phil Howell <phil.howell@erskinedesign.com>
2011-07-19 13:44:45 +0100

    Updated free memory calculation to exclude cached and buffered memory Added websocket configuration value use in front-end

--- config.example.js
+++ config.example.js
@@ -5,16 +5,19 @@ config.servers = [
 //    'hostname'
 ];
 
+// Webserver configuration
 config.web = {
     'port' : 3000,
     'host' : 'localhost'
 };
 
+// Websocket server configuration
 config.ws = {
     'port' : 8000,
     'host' : 'localhost'
 };
 
+// Message queue server configuration
 config.amqp = {
     'host' : 'localhost'
 }
--- monitor.js
+++ monitor.js
@@ -14,7 +14,7 @@ web.use('/static/', express.static(__dirname + '/static'));
 web.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 web.get('/', function(req, res){
-    res.render('index.ejs', {layout: false, servers: config.servers});
+    res.render('index.ejs', {layout: false, servers: config.servers, websocket_host: config.ws.host});
 });
 
 web.listen(config.web.port, config.web.host);
@@ -45,18 +45,17 @@ connection.on('ready', function () {
         stats = JSON.parse(data);
         
         // Maybe move this to the poller, trying to keep it as generic as possible at the moment
-        if (typeof stats.memory_total !== 'undefined' && typeof stats.memory_free !== 'undefined') {
+        if (typeof stats.memory_total !== 'undefined' && typeof stats.memory_free !== 'undefined' && typeof stats.memory_buffered !== 'undefined' && typeof stats.memory_cached !== 'undefined') {
             stats.memory = {};
-            stats.memory.low = parseInt((stats.memory_total/3)/1024);
-            stats.memory.high = stats.memory.low * 2;
+            stats.memory.low = parseInt((stats.memory_total/2)/1024);
+            stats.memory.high = parseInt((stats.memory_total*0.75)/1024);
             stats.memory.total = parseInt(stats.memory_total/1024);
-            stats.memory.used = stats.memory.total - parseInt(stats.memory_free/1024);
-            delete stats.memory_free;
-            delete stats.memory_total;
-        } else {
-            delete stats.memory_free;
-            delete stats.memory_total;
+            stats.memory.used = stats.memory.total - (parseInt(stats.memory_free/1024) + parseInt(stats.memory_buffered/1024) + parseInt(stats.memory_cached/1024));
         }
+        delete stats.memory_free;
+        delete stats.memory_buffered;
+        delete stats.memory_cached;
+        delete stats.memory_total;
 
         // Easier than combining sys, user and nice load
         stats.cpu_use = 100 - parseInt(stats.cpu_idle);
