58a0de8e45dd8bc61f645a0af5d7c77ffe609844
Phil Howell <phil@quae.co.uk>
2014-11-01 01:44:17 +0000

    Bring inline with library changes

--- monitor.js
+++ monitor.js
@@ -1,71 +1,76 @@
 var sys = require('sys'),
     amqp = require('amqp'),
-    ws = require('websocket-server'),
+    ws = require('ws').Server,
+    errorhandler = require('errorhandler'),
     express = require('express'),
     config = require('./config');
 
 // Express
-var web = express.createServer();
+var web = express();
 
 web.set('view engine', 'ejs');
 
 // Should wrap in a node development environment check
 web.use('/static/', express.static(__dirname + '/static'));
-web.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+web.use(errorhandler());
 
 web.get('/', function(req, res){
     res.render('index.ejs', {layout: false, servers: config.servers, websocket_host: config.ws.host});
 });
 
 web.listen(config.web.port, config.web.host);
+var socket_server = new ws({port: config.ws.port});
 
-var socket_server = ws.createServer();
+socket_server.broadcast = function(data) {
+
+    var json_data = JSON.stringify(data);
+
+    for(var i in this.clients) {
+        this.clients[i].send(json_data);
+    }
+
+};
 
 // Define AMQP (RabbitMQ) connection options
-var connection = amqp.createConnection({
-    host: config.amqp.host,
-});
+var connection = amqp.createConnection({host: config.amqp.host});
 
 // Wait for connection to become established.
 connection.on('ready', function () {
 
     // Create servers exchange if it doesn't exist
-    var servers_exch = connection.exchange('servers')
+    // var exchange = connection.exchange('servers', {type: 'topic'});
 
-    // Create queue and bind
-    var q = connection.queue('server-stats', function(queue) {
+    var queue = connection.queue('server-stats', function(q) {
+
+        // Create queue and bind
         q.bind('servers', 'stats.#');
+        // Receive messages
+        q.subscribe(function (msg) {
+
+            // Marshal received string into JSON
+            data = msg.data.toString('utf8');
+            stats = JSON.parse(data);
+
+            // Maybe move this to the poller, trying to keep it as generic as possible at the moment
+            if (typeof stats.memory_total !== 'undefined' && typeof stats.memory_free !== 'undefined' && typeof stats.memory_buffered !== 'undefined' && typeof stats.memory_cached !== 'undefined') {
+                stats.memory = {};
+                stats.memory.low = parseInt((stats.memory_total/2)/1024, 10);
+                stats.memory.high = parseInt((stats.memory_total*0.75)/1024, 10);
+                stats.memory.total = parseInt(stats.memory_total/1024, 10);
+                stats.memory.used = stats.memory.total - (parseInt(stats.memory_free/1024, 10) + parseInt(stats.memory_buffered/1024, 10) + parseInt(stats.memory_cached/1024, 10));
+            }
+            delete stats.memory_free;
+            delete stats.memory_buffered;
+            delete stats.memory_cached;
+            delete stats.memory_total;
+
+            // Easier than combining sys, user and nice load
+            stats.cpu_use = 100 - parseInt(stats.cpu_idle, 10);
+            delete stats.cpu_idle;
+
+            // Relay JSON via websocket server (as string)
+            socket_server.broadcast(stats);
+        });
     });
 
-    // Receive messages
-    q.subscribe(function (msg) {
-
-        // Marshal received string into JSON
-        data = msg.data.toString('utf8');
-        stats = JSON.parse(data);
-        
-        // Maybe move this to the poller, trying to keep it as generic as possible at the moment
-        if (typeof stats.memory_total !== 'undefined' && typeof stats.memory_free !== 'undefined' && typeof stats.memory_buffered !== 'undefined' && typeof stats.memory_cached !== 'undefined') {
-            stats.memory = {};
-            stats.memory.low = parseInt((stats.memory_total/2)/1024);
-            stats.memory.high = parseInt((stats.memory_total*0.75)/1024);
-            stats.memory.total = parseInt(stats.memory_total/1024);
-            stats.memory.used = stats.memory.total - (parseInt(stats.memory_free/1024) + parseInt(stats.memory_buffered/1024) + parseInt(stats.memory_cached/1024));
-        }
-        delete stats.memory_free;
-        delete stats.memory_buffered;
-        delete stats.memory_cached;
-        delete stats.memory_total;
-
-        // Easier than combining sys, user and nice load
-        stats.cpu_use = 100 - parseInt(stats.cpu_idle);
-        delete stats.cpu_idle;
-        
-        // Relay JSON via websocket server (as string)
-        //console.log(stats);
-        socket_server.broadcast(JSON.stringify(stats));
-
-    });
 });
-
-socket_server.listen(config.ws.port, config.ws.host);
